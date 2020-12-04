3451d178b5be3cdfe2a9f24561332d65abc2cbe1
winds <a0d6m1i3n@gmail.com>
2020-12-04 22:06:20 +0800

    latest

--- build/ios/Debug-iphonesimulator/multi_select_item/multi_select_item.framework/Headers/MultiSelectItemPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface MultiSelectItemPlugin : NSObject<FlutterPlugin>
-@end
--- build/ios/Debug-iphonesimulator/orientation/orientation.framework/Headers/OrientationPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface OrientationPlugin : NSObject<FlutterPlugin, FlutterStreamHandler>
-@end
