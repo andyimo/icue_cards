9480fa9588dceaf525cb4ce56431d801f8b15add
andyimo <andii.trann@outlook.com>
2020-12-04 14:12:30 -0500

    Created 2 decks

--- lib/views/dir_root.dart
+++ lib/views/dir_root.dart
@@ -37,32 +37,151 @@ class _MainDirectoryState extends State<MainDirectory> {
     folders.add(folder3);
     folders.add(folder4);
 
-    Deck deck = Deck("Chapter 1");
+    Deck deck = Deck("Development Processes");
     Deck deck2 = Deck("Chapter 2");
     Deck deck3 = Deck("Chapeter 3");
     Deck deck4 = Deck("Chapter 4");
+    Deck deck5 = Deck("Intro to Design Patterns");
 
     folder.addDeck(deck);
     folder.addDeck(deck2);
     folder.addDeck(deck3);
     folder.addDeck(deck4);
+    folder.addDeck(deck5);
+
+    deck5.addCard(new iCueCard(
+        frontSide: 'Design Pattern',
+        backSide:
+            'Typical solution to common problems in software design.\nThink of it like a blueprint you can customize to solve a particular design problem in your code.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Creational Design Pattern',
+        backSide:
+            'Design pattern that provides various object cretion mechanisms, which increase flexibility and reuse of existing code.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Structural Design Pattern',
+        backSide:
+            'Design pattern that explains how to assemble objects and classes into larger structures while keeping these structures flexible and efficient'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Behavioural Design Pattern',
+        backSide:
+            'Design pattern concerned with algorithms and assignment of responsibility between objects.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Singleton',
+        backSide:
+            'Creational Design Pattern.\nLets you ensure that a class has only one instance, while providing a global access point to this instance.\nWhy use it: Provides stricter access to shared resources (like a database or a file). Great if clients should all be sharing a single instance of a class.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Observer',
+        backSide:
+            'Behavioural Design Pattern.\n(aka. Event-Subscriber, Listener)\nLets you define a subscription mechanism to notify multiple objects about any events that happen to the object they are observing.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Composite',
+        backSide:
+            'Structural Design Pattern.\nLets you compose objects into tree structures and then work with these structures as if they were individual objects. This pattern is useful if you want the client code to treat both simple and complex elements uniformly.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Strategy',
+        backSide:
+            'Behavioural Design Pattern.\nLets you define a family of algorithms, put each of them into a separate class, and make their objects interchangeable.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Adaptor',
+        backSide:
+            'Structural Design Pattern.\n(aka. Wrapper)\nAllows objects with incompatible interfaces to collaborate. (e.g. a wrapper that converts XML files into JSON format to be passed into an Analytics library)'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Facade',
+        backSide:
+            'Structural Design Pattern.\nProvides a simplified interface to a library, frameowkr or any complex set of classes. Use when you need an interface that is more straightforward and to the point.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Template Method',
+        backSide:
+            'Behavioural Design Pattern.\nDefines the skeleton of an algorithm in the superclass but lets subclasses override specific steps of the algorithm without changing its structure. Use when several classes have similar algorithms with minor differences.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Iterator',
+        backSide:
+            'Behavioural Design Pattern.\nLets you traverse elements of a collection without exposing its underlying representation (list, stack, tree, etc.). The main idea is to extract the traversal behaviour of a collection into a separate object.'));
+    deck5.addCard(new iCueCard(
+        frontSide: 'Decorator',
+        backSide:
+            'Structural Design Pattern.\nLets you attach new behaviours to objects by replacing these objects inside special wrapper objects that contain the behaviours. e.g. Assign extra behaviours to objects at runtime without breaking the code that uses these objects.'));
 
     deck2.addCard(new iCueCard(
         frontSide: 'chapter 2 sample question',
         backSide: 'chapter 2 sample answer',
         color: Colors.green));
+
     deck.addCard(new iCueCard(
-        frontSide: 'sample question1',
-        backSide: 'sample answer1',
+        frontSide: 'Software Engineering Process',
+        backSide:
+            'Template for organizing tasks and people in a project. Define who does what, when and how in the development of a software system. Has roles, workflows, milestones and guidelines.',
         color: Colors.green));
     deck.addCard(iCueCard(
-        frontSide: "sample question2",
-        backSide: 'sample answer2',
+        frontSide: "Lightweight Process",
+        backSide:
+            ' Type of Software Engineering Process with focus on working code rather than documentation/Direct communication between client and developer/Low management overhead',
         color: Colors.amber));
     deck.addCard(new iCueCard(
-        frontSide: 'sample question3',
-        backSide: 'sample answer3',
+        frontSide: 'Heavyweight Process',
+        backSide:
+            'Type of Software Engineering Process that is document driven, has many different roles, checkpoints, highly bureaucratic',
         color: Colors.blue));
+    deck.addCard(new iCueCard(
+        frontSide: 'Waterfall',
+        backSide:
+            'Linear, sequential Software Engineering Process with 5 phases: Requirements, Design, Implementation, Verification, Maintenance. Very costly to revisit a previous stage.'));
+    deck.addCard(new iCueCard(
+        frontSide: 'Spiral',
+        backSide:
+            'Iterative Software Engineering Process. Used for high risk projects. This process is divided into 4 quadrants (Identify objective and constraints, Analyze and mitigate risks, Execution and testing, Review Progress and plan for next phase).'));
+    deck.addCard(new iCueCard(
+        frontSide: 'RUP (Rational Unified Process)',
+        backSide:
+            'Iterative Software Engineering Process created by Rational Software Corporation (IBM). Use case driven and architecture centric. Contains 4 phases(Inception, Elaboration, Construction, Transition)'));
+    deck.addCard(new iCueCard(
+        frontSide: '4+1 View Model',
+        backSide:
+            'Defines the architecture of a system from 5 perspectives (Implementation/Development view, Logical/Analytical View, Physical/Deployment view, Process view, Use-Case view)'));
+    deck.addCard(new iCueCard(
+        frontSide: 'Agile',
+        backSide:
+            'Aka lightweight process. Can accomodate changing requirements, Customer collaboration, has less documentation'));
+    deck.addCard(new iCueCard(
+        frontSide: 'XP (Xtreme Programming)',
+        backSide:
+            'An Agile software engineering process. No design up front. Write test cases before writing code. Pair programming + refactoring.'));
+    deck.addCard(new iCueCard(
+        frontSide: 'The Planning Game',
+        backSide:
+            '[XP] Stakeholder meeting to plan the next iteration. Business people decide on business values of features. Developers on the technical risk of features and predicted effort per feature.'));
+    deck.addCard(new iCueCard(
+        frontSide: 'Pair Programming',
+        backSide:
+            'All production code written by 2 programmers. One programmer is thinking about the implementation of a method while the other thinks strategically about the whole system. Pairs are put together and shuffled dynamically.'));
+    deck.addCard(new iCueCard(
+        frontSide: 'SCRUM',
+        backSide:
+            'An agile process. Based on Rugby. Emphasis on helping teams work together. Encourages team to learn through experiences, self-organize while working on a problem, and reflect on the successes and failures to continuously improve. The three main roles in are the ScrumMaster, Product owner, team of 3-9 people.'));
+    deck.addCard(new iCueCard(
+        frontSide: 'Scrum Master',
+        backSide:
+            'Acts as a facilitator, distraction remover, proces cop. Is NOT the team leader. '));
+    deck.addCard(new iCueCard(
+        frontSide: 'Sprints',
+        backSide:
+            'An iteration of SCRUM. At the end of every sprint, the team has a shippable product that is demoed to the product owner. Update sprint backlog. Happens every 2-4 weeks. '));
+    deck.addCard(new iCueCard(
+        frontSide: 'Product Backlog',
+        backSide:
+            'SCRUM artifact. High level document created for the entire project. Contains the list of features to be implemented with an associated priority/business value and a development effort value.'));
+    deck.addCard(new iCueCard(
+        frontSide: 'Sprint Backlog',
+        backSide:
+            'SCRUM artifact. Contains info about how the team is going to implement features for an upcoming sprint. Features are broken down into tasks (about 4-6 hrs of work), team members sign up for tasks voluntarily. A Task Board monitors the progress of each task'));
+    deck.addCard(new iCueCard(
+        frontSide: 'Burn Down Chart',
+        backSide:
+            'SCRUM artifact. Shows the remaining work in the sprint backlog (updated daily).'));
+    deck.addCard(new iCueCard(
+        frontSide: 'Daily Scrum',
+        backSide:
+            ' A daily project status meeting about 15 imuntes (standing up). 3 questions: What have you done? what are you working on today? Any challenges?'));
     controller.set(folders.length);
   }
 
