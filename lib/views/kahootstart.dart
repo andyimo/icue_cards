ae602a2b86efbd0e93f57a4100e48061d249fc63
shuqiYang1112 <63683638+shuqiYang1112@users.noreply.github.com>
2020-12-06 21:30:49 +0800

    Update kahootstart.dart

--- lib/views/kahootstart.dart
+++ lib/views/kahootstart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:audioplayers/audio_cache.dart';
 //import 'package:audioplayers/audioplayers.dart';
 import 'Kahoot.dart';
+//import 'dart:async';
 
 class KahootStart extends StatefulWidget {
   @override
@@ -13,7 +14,8 @@ class _KahootStartState extends State<KahootStart> {
   String _dropClassValue = "Math";
   int _dropNumValue = 5;
   int index = 0;
-  int _currentIndex = 0;
+  int timer = 1;
+  double mCurrentValue = 1.0;
   // AudioPlayer audioPlayer = AudioPlayer();
 
   @override
@@ -50,6 +52,44 @@ class _KahootStartState extends State<KahootStart> {
                   _dropNumValue = value;
                 });
               }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Countdown： Yes"),
+              Radio(
+                value: 1,
+                groupValue: this.timer,
+                onChanged: (value) {
+                  setState(() {
+                    this.timer = value;
+                  });
+                },
+              ),
+              SizedBox(width: 20),
+              Text("No："),
+              Radio(
+                value: 2,
+                groupValue: this.timer,
+                onChanged: (value) {
+                  setState(() {
+                    this.timer = value;
+                  });
+                },
+              )
+            ],
+          ),
+          Slider(
+              value: mCurrentValue,
+              min: 1,
+              max: 10,
+              label: '$mCurrentValue',
+              divisions: 10,
+              onChanged: (e) {
+                setState(() {
+                  //四舍五入的双精度值
+                  mCurrentValue = e.roundToDouble();
+                });
+              }),
           new RaisedButton(
             child: Text('Start'),
             color: Colors.blueAccent[600],
@@ -57,7 +97,10 @@ class _KahootStartState extends State<KahootStart> {
               context,
               MaterialPageRoute(
                   builder: (context) => Kahoot(
-                      category: _dropClassValue, questionNum: _dropNumValue)),
+                      category: _dropClassValue,
+                      questionNum: _dropNumValue,
+                      timer: timer,
+                      mCurrentValue: mCurrentValue)),
             ),
           ),
         ]),
