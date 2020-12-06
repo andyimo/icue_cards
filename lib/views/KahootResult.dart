3e3ec3abe8b56b5c85b3ca1eaa85376dce0c0489
shuqiYang1112 <63683638+shuqiYang1112@users.noreply.github.com>
2020-12-06 21:27:28 +0800

    Update KahootResult.dart

--- lib/views/KahootResult.dart
+++ lib/views/KahootResult.dart
@@ -1,37 +1,48 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'KahootReview.dart';
-import '../models/iCueCard.dart';
+import 'iCuecard.dart';
 import 'Kahootstart.dart';
-import '../screens/home/home.dart';
+import 'KahootResult.dart';
 
-class KahootResult extends StatelessWidget {
-  final String score;
+class Review extends StatefulWidget {
   final List<iCueCard> wronglist;
-  KahootResult({this.score, this.wronglist});
+
+  //var strtime;
+  Review({this.wronglist});
+  @override
+  _ReviewState createState() => _ReviewState(this.wronglist);
+}
+
+class _ReviewState extends State<Review> {
+  List<iCueCard> _wronglist;
+
+  //var _strtime;
+  _ReviewState(List<iCueCard> wronglist) {
+    _wronglist = wronglist;
+
+    //_strtime = strtime;
+  }
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Kahoot Result"), actions: [
-          IconButton(
-            icon: Icon(Icons.home),
-            onPressed: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Home())),
-          )
-        ]),
-        body: Center(
-          child: new Column(
-            children: <Widget>[
+    //print(_wronglist.length);
+    if (_wronglist.length == 0) {
+      return Scaffold(
+          appBar: AppBar(
+              title: Text(
+            "Review",
+          )),
+          body: Center(
+            child: Column(children: [
               Text(
                 "   ",
                 style: TextStyle(
                   fontSize: 30,
                 ),
               ),
-              Text(
-                "Your grade is $score !",
+              new Text(
+                "Nothing to review!",
                 style: TextStyle(
                   fontSize: 30,
                   color: Colors.blue,
@@ -43,22 +54,99 @@ class KahootResult extends StatelessWidget {
                   fontSize: 30,
                 ),
               ),
-              new RaisedButton(
-                child: Text('Back to Start page!'),
-                color: Colors.blueAccent[600],
+              RaisedButton(
+                child: Text(
+                  'Back to Start page!',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
                 onPressed: () => Navigator.push(context,
                     MaterialPageRoute(builder: (context) => KahootStart())),
               ),
-              new RaisedButton(
-                child: Text('Review Wrong Question!'),
-                color: Colors.blueAccent[600],
-                onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => Review(wronglist: wronglist))),
+            ]),
+          ));
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Review"),
+        ),
+        body: ConstrainedBox(
+          //ConstrainedBox(
+          constraints: BoxConstraints.expand(),
+          child: Stack(
+            alignment: Alignment.center, //指定未定位或部分定位widget的对齐方式
+            children: <Widget>[
+              Container(
+                //color: Colors.blue,
+                height: 430,
+                width: 270,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '${_wronglist[index].getFront()}',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    Container(width: 80.0, height: 15.0),
+                    Divider(
+                      height: 2.0,
+                      color: Colors.grey[700],
+                    ),
+                    Container(width: 80.0, height: 15.0),
+                    Text(
+                      '${_wronglist[index].getBack()}',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 30.0,
+                child: Row(children: <Widget>[
+                  RaisedButton(
+                    child: Text('Prev'),
+                    onPressed: () {
+                      setState(() {
+                        if (index > 0) {
+                          index--;
+                        }
+                      });
+                    },
+                  ),
+                  Container(width: 10.0, height: 1.0),
+                  RaisedButton(
+                    child: Text('Back to Result page!'),
+                    onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => KahootResult())),
+                  ),
+                  Container(width: 10.0, height: 1.0),
+                  RaisedButton(
+                    child: Text('Next'),
+                    onPressed: () {
+                      setState(() {
+                        if (index < _wronglist.length - 1) {
+                          index++;
+                        }
+                      });
+                    },
+                  ),
+                ]),
               ),
             ],
           ),
-        ));
+        ),
+      );
+    }
   }
 }
