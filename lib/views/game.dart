a91b3e274ed5bb57491f9dc2e0350d5729886de2
shuqiYang1112 <63683638+shuqiYang1112@users.noreply.github.com>
2020-12-06 21:33:25 +0800

    Update game.dart

--- lib/views/game.dart
+++ lib/views/game.dart
@@ -1,57 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:icue_cards/screens/home/home.dart';
+import 'package:../screens/home/home.dart';
+
 import 'headsupstart.dart';
 import 'Kahootstart.dart';
-import '../models/root.dart';
+//import 'Home.dart';
 
-/*void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: GamePage(title: 'Game Page'),
-    );
-  }
-}*/
 
 class GamePage extends StatefulWidget {
-  final Root root;
-  GamePage({this.root});
+  GamePage({Key key, this.title}) : super(key: key);
+  final String title;
 
   @override
-  _GamePageState createState() => _GamePageState(root: this.root);
+  _GamePageState createState() => _GamePageState();
 }
 
 class _GamePageState extends State<GamePage> {
-  final Root root;
-  _GamePageState({this.root});
+  //AudioPlayer audioPlugin = AudioPlayer();
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Game Page"), actions: [
+      appBar: AppBar(title: Text('Game Page'), actions: <Widget>[
         IconButton(
           icon: Icon(Icons.home),
           onPressed: () => Navigator.push(
               context, MaterialPageRoute(builder: (context) => Home())),
-        )
+        ),
       ]),
       body: Center(
         child: new Column(
           children: <Widget>[
             Container(width: 80.0, height: 20.0),
             new RaisedButton(
-              child: Text('Heads Up!'),
+              child: Text('Heads Up Game'),
               color: Colors.blueAccent[600],
               onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => HeadsUpStart(root: root))),
+                  MaterialPageRoute(builder: (context) => HeadsUpStart())),
             ),
             new RaisedButton(
-              child: Text('Kahoot!'),
+              child: Text('Kahoot Game'),
               color: Colors.blueAccent[600],
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => KahootStart())),
