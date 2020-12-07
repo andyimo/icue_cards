295409904de936a3247d1dd245887c20454262b1
andyimo <andii.trann@outlook.com>
2020-12-06 20:35:12 -0500

    dev log update

--- lib/views/HeadsUpResult.dart
+++ lib/views/HeadsUpResult.dart
@@ -5,7 +5,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'MistakesReview.dart';
-import 'headsupstart.dart';
 import '../models/root.dart';
 
 class HeadsUpResult extends StatefulWidget {
@@ -15,8 +14,8 @@ class HeadsUpResult extends StatefulWidget {
   HeadsUpResult({this.root, this.score, this.mislst});
 
   @override
-  _HeadsUpResultState createState() =>
-      _HeadsUpResultState(root: this.root, score: this.score, mislst: this.mislst);
+  _HeadsUpResultState createState() => _HeadsUpResultState(
+      root: this.root, score: this.score, mislst: this.mislst);
 }
 
 class _HeadsUpResultState extends State<HeadsUpResult> {
@@ -37,23 +36,25 @@ class _HeadsUpResultState extends State<HeadsUpResult> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          AppBar(
-            automaticallyImplyLeading: false, 
-            title: Text("Game Result"),
-            actions: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(right:20.0),
-                child: GestureDetector(
-                  onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context)=>Home()));},
-                  child: Icon(
-                    Icons.home,
-                    size: 26.0,
-                  ),
-                ),
-                ),
-            ],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text("Game Result"),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Home()));
+              },
+              child: Icon(
+                Icons.home,
+                size: 26.0,
+              ),
+            ),
           ),
+        ],
+      ),
       body: Center(
         child: Column(
           children: [
@@ -75,14 +76,15 @@ class _HeadsUpResultState extends State<HeadsUpResult> {
                 fontSize: 80.0,
                 //color:Colors.yellow,
               ),
-              ),
+            ),
             new RaisedButton(
               //for the replay the game
               child: Text('Play it Again'),
               color: Colors.blueAccent[600],
               onPressed: () => Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => HeadsUpStart(root: root)),
+                MaterialPageRoute(
+                    builder: (context) => HeadsUpStart(root: root)),
               ),
             ),
             new RaisedButton(
